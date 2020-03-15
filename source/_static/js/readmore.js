0f42accc6db2615b17939e4f0b7d328c398a5cfe
BingmingWong <wongbingming@163.com>
2020-03-15 16:35:04 +0800

    update: 获取 vip 验证码

--- source/_static/js/readmore.js
+++ source/_static/js/readmore.js
@@ -39,8 +39,8 @@ var setIdTimer = setInterval(function () {
             btw.init({
                 id: id,
                 blogId: '15406-1578143418297-890',
-                name: 'Python编程时光',
-                qrcode: 'http://image.python-online.cn/20200104210733.png',
+                name: 'Go编程时光',
+                qrcode: 'http://image.python-online.cn/20200315163037.png',
                 keyword: 'vip'
             });
         }
